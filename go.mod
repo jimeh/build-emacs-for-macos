8d87c01db79201182fbcd1a210b1b19df9209aeb
Jim Myhrberg <contact@jimeh.me>
2021-05-23 18:27:13 +0100

    feat(cli): add basis for new "emacs-builder" CLI tool written in Go
    This will eventually replace the build-emacs-for-macos script that's
    written in Ruby. The migration of functionality will happen bit by bit
    over time.

---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/jimeh/build-emacs-for-macos
+
+go 1.16
+
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.0 // indirect
+	github.com/fatih/color v1.12.0 // indirect
+	github.com/hashicorp/go-hclog v0.16.1
+	github.com/mattn/go-isatty v0.0.13 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/stretchr/testify v1.6.1 // indirect
+	github.com/urfave/cli/v2 v2.3.0
+	golang.org/x/sys v0.0.0-20210603125802-9665404d3644 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+)
---
+++ pkg/cli/cli.go
@@ -0,0 +1,89 @@
+package cli
+
+import (
+	"fmt"
+	"strings"
+
+	cli2 "github.com/urfave/cli/v2"
+)
+
+type CLI struct {
+	App     *cli2.App
+	Version string
+	Commit  string
+	Date    string
+}
+
+func New(version, commit, date string) *CLI {
+	if version == "" {
+		version = "0.0.0-dev"
+	}
+
+	c := &CLI{
+		Version: version,
+		Commit:  commit,
+		Date:    date,
+		App: &cli2.App{
+			Name:                 "emacs-builder",
+			Usage:                "Tool to build emacs",
+			Version:              version,
+			EnableBashCompletion: true,
+			Flags: []cli2.Flag{
+				&cli2.StringFlag{
+					Name:    "log-level",
+					Usage:   "set log level",
+					Aliases: []string{"l"},
+					Value:   "info",
+				},
+				&cli2.BoolFlag{
+					Name:    "quiet",
+					Usage:   "silence noisy output",
+					Aliases: []string{"q"},
+					Value:   false,
+				},
+				cli2.VersionFlag,
+			},
+			Commands: []*cli2.Command{
+				{
+					Name:    "version",
+					Usage:   "print the version",
+					Aliases: []string{"v"},
+					Action: func(c *cli2.Context) error {
+						cli2.VersionPrinter(c)
+
+						return nil
+					},
+				},
+			},
+		},
+	}
+
+	cli2.VersionPrinter = c.VersionPrinter
+
+	return c
+}
+
+func (s *CLI) VersionPrinter(c *cli2.Context) {
+	version := c.App.Version
+	if version == "" {
+		version = "0.0.0-dev"
+	}
+
+	extra := []string{}
+	if len(s.Commit) >= 7 {
+		extra = append(extra, s.Commit[0:7])
+	}
+	if s.Date != "" {
+		extra = append(extra, s.Date)
+	}
+	var extraOut string
+	if len(extra) > 0 {
+		extraOut += " (" + strings.Join(extra, ", ") + ")"
+	}
+
+	fmt.Printf("%s%s\n", version, extraOut)
+}
+
+func (s *CLI) Run(args []string) error {
+	return s.App.Run(args)
+}
