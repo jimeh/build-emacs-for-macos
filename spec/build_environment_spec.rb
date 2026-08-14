5252befe62b786596eab6e3687cd2bf8262a2aba
Jim Myhrberg <contact@jimeh.me>
2026-08-14 23:21:09 +0100

    test: make Homebrew selection spec portable

--- spec/build_environment_spec.rb
+++ spec/build_environment_spec.rb
@@ -94,14 +94,16 @@ RSpec.describe 'Build environment' do
 
   it 'selects versioned Homebrew Tree-sitter pkg-config metadata ' \
      'by Emacs major' do
+    allow(OS).to receive(:version).and_raise('OS.version must not be used')
+
     with_build_environment(use_nix: false, ref: 'emacs-30.2') do |build, *, **|
-      expect(build.send(:env_PKG_CONFIG_PATH).first).to eq(
+      expect(build.send(:tree_sitter_pkg_config_path)).to eq(
         '/homebrew/opt/tree-sitter@0.25/lib/pkgconfig'
       )
     end
 
     with_build_environment(use_nix: false, ref: 'emacs-31') do |build, *, **|
-      expect(build.send(:env_PKG_CONFIG_PATH).first).to eq(
+      expect(build.send(:tree_sitter_pkg_config_path)).to eq(
         '/homebrew/opt/tree-sitter/lib/pkgconfig'
       )
     end
